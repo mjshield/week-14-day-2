c6624d8cc129ae261b74100428d5353fd434aab3
Matthew Shield <matthewshield@hotmail.co.uk>
2017-09-26 14:42:52 +0100

    Initial commit

---
+++ client/src/containers/FilmContainer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FilmSelector from '../components/FilmSelector'
+import FilmDetail from '../components/FilmDetail'
+
+class FilmContainer extends Component {
+
+
+    
+
+
+    render() {
+
+        return (
+            <div>
+                <h2>Sutherland Films</h2>
+            </div>
+        )
+    }
+
+}
+
+export default FilmContainer;
